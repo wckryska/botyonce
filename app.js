b33038640dc062f9e800368f5dcef75adab25a57
wckryska <wckryska@umich.edu>
2016-10-09 03:45:32 -0400

    Update app.js
    deleted lowercase port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
